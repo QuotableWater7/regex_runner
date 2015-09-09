bf043090390690ec4721202f43dd96d0376fcf09
JT <jbowler2400@gmail.com>
2015-09-09 08:35:24 -0700

    some refactoring

--- fsmsim.js
+++ fsmsim.js
@@ -1,20 +1,23 @@
 var _ = require('underscore');
 
 function fsmsim(string, current_node, edges, accepting_states) {
-  if (_.isEmpty(string)) { return _.contains(accepting_states, current_node); }
-
-  var char = string[0];
-  var rest_of_string = _.rest(string);
-  var next_node = edges[current_node + char];
+  var next_node = edges[current_node + string[0]];
   var epsilon_node = edges[current_node + 'epsilon'];
+  var state = [string, current_node, edges, accepting_states];
 
-  if (!(next_node || epsilon_node)) { return false; }
+  if (!epsilon_node) {
+    if (_.isEmpty(string)) {
+      return _.contains(accepting_states, current_node);
+    } else if (!next_node) {
+      return false;
+    }
+  }
 
   var epsilon_path_result = false;
   if (epsilon_node) {
     epsilon_path_result = fsmsim(string, epsilon_node, edges, accepting_states);
   }
-  var regular_path_result = fsmsim(rest_of_string, next_node, edges, accepting_states);
+  var regular_path_result = fsmsim(_.rest(string), next_node, edges, accepting_states);
 
   return regular_path_result || epsilon_path_result;
 }
--- runner.js
+++ runner.js
@@ -4,35 +4,13 @@ var Token = require('./token');
 var _ = require('underscore');
 var tokenBuilder = require('./token_builder');
 
-// var tokens = [
-//   new Token('a'),
-//   new Token('c')
-// ];
-// var edges = {
-//   '1a': 2,
-//   '2b': 3
-// };
-// var accepting_states = [3];
-// console.log(fsmsim(tokens, 1, edges, accepting_states));
+var args = process.argv.slice(2);
 
-var pattern = 'ab?c';
+var string_to_match = args[0];
+var pattern = args[1];
 var tokens = tokenBuilder(pattern);
 var edges = edgeBuilder(tokens);
-var output = fsmsim('abcd', 1, edges, [4]);
+var accepting_states = [tokens.length + 1];
 
+var output = fsmsim(string_to_match , 1, edges, accepting_states);
 console.log(output);
-
-
-// FSMSIM tests
-// var edges = {
-//   '1a': 2,
-//   '1b': 3,
-//   '2c': 4,
-//   '3c': 4
-// };
-// var accepting_states = [4];
-// var test_strings = ['ac', 'bc', 'whatever'];
-
-// _.each(test_strings, function (test_string) {
-//   console.log(fsmsim(test_string, 1, edges, accepting_states));
-// });
